ee947e65e9102ec96dec4f39156f69f6053a2375
giovany303 <giovany@303software.com>
2018-01-27 13:16:39 -0600

    getting data

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDom from "react-dom";
+import axios from "axios";
+//import People from './cars';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {cars: []}
+    }
+
+    componentDidMount(){
+        var _this = this;
+        axios.get('https://swapi.co/api/vehicles/').then(function(results){
+            _this.setState({
+                cars: results.data.results
+            });            
+        })
+    }
+
+    render(){
+        return(
+            <div></div>
+        );
     }
 }
 
